c793a5ac8f3f501c6c3a6497cf2efb221c4f008d
caster <2474252111@qq.com>
2022-04-26 18:19:45 +0800

    [新增] 增加OnGeneratedCSProject回调 将unity项目 生成dll 路径固定

---
+++ Assets/Scripts/Editor/AssetPostProcessor/OnGenerateCSProjectProcessor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UGFExtensions
+{
+    public class OnGenerateCSProjectProcessor : AssetPostprocessor
+    {
+        public static string OnGeneratedCSProject(string path, string content)
+        {
+            if (path.EndsWith("Assembly-CSharp.csproj"))
+            {
+                return content.Replace("<OutputPath>Temp\\Bin\\Debug\\Assembly-CSharp\\</OutputPath>", "<OutputPath>Temp\\Bin\\Debug\\</OutputPath>");
+            }
+            return content;
+        }
+    }
+}
